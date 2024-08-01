cecc71a997b3886167de10ec309e39945c560a81
Valentina Benitez <valentinabenitezzapata@gmail.com>
2024-08-01 10:48:31 -0500

    Cambios en la función home

--- movie/views.py
+++ movie/views.py
@@ -5,6 +5,6 @@ from django.http import HttpResponse
 
 def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
-    return render(request, 'home.html', {'name': 'Greg Lim'})
+    return render(request, 'home.html', {'name': 'Valentina Benitez Zapata'})
 def about(request):
     return HttpResponse('<h1>Welcome to About Page</h1>')
