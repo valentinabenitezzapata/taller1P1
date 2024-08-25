25249e407ee8cf24e2a471163bd6c19205182aa5
Valentina Benitez <valentinabenitezzapata@gmail.com>
2024-08-25 13:48:29 -0500

    workshop 2 extra class

--- movie/views.py
+++ movie/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Movie
+import matplotlib.pyplot as plt
+import matplotlib
+import io
+import urllib, base64
 
 # Create your views here.
 
@@ -16,5 +20,66 @@ def home(request):
 def about(request):
     #return HttpResponse('<h1>Welcome to About Page</h1>')
     return render(request, 'about.html')
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email': email})
+def statistics_view(request):
+    matplotlib.use('Agg')
+    all_movies = Movie.objects.all()
+    movie_counts_by_year = {}
+    for movie in all_movies:
+        year = movie.year if movie.year else 'None'
+        if year in movie_counts_by_year:
+            movie_counts_by_year[year] += 1
+        else:
+            movie_counts_by_year[year] = 1
+
+    bar_width = 0.5
+    bar_positions = range(len(movie_counts_by_year))
+    
+    plt.bar(bar_positions, movie_counts_by_year.values(), width=bar_width, align='center')
+    plt.title('Movies per year')
+    plt.xlabel('Year')
+    plt.ylabel('Number of movies')
+    plt.xticks(bar_positions, movie_counts_by_year.keys(), rotation=90)
+    plt.subplots_adjust(bottom=0.3)
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+    plt.close()
+
+    image_png = buffer.getvalue()
+    buffer.close()
+    graphic = base64.b64encode(image_png)
+    graphic = graphic.decode('utf-8')
+
+    #genre
+    count_by_genre = {}
+    for movie in all_movies:
+        genre = movie.genre.split(',')[0] if movie.genre else 'None'
+        if genre in count_by_genre:
+            count_by_genre[genre] += 1
+        else:
+            count_by_genre[genre] = 1
+    
+    bar_positions_genre = range(len(count_by_genre))
+    
+    plt.bar(bar_positions_genre, count_by_genre.values(), width=bar_width, align='center')
+    plt.title('Movies per genre')
+    plt.xlabel('Genre')
+    plt.ylabel('Number of movies')
+    plt.xticks(bar_positions_genre, count_by_genre.keys(), rotation=90)
+    plt.subplots_adjust(bottom=0.3)
+    buffer_genre = io.BytesIO()
+    plt.savefig(buffer_genre, format='png')
+    buffer_genre.seek(0)
+    plt.close()
+
+    image_png_genre = buffer_genre.getvalue()
+    buffer_genre.close()
+    graphic_genre = base64.b64encode(image_png_genre)
+    graphic_genre = graphic_genre.decode('utf-8')
+
+    return render(request, 'statistics.html', {'graphic': graphic, 'graphic_genre': graphic_genre})
 
     
--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -25,7 +25,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', movieViews.home, name='home'),
     path('about/', movieViews.about, name='about'),
-    path('news/', include('news.urls'))
+    path('news/', include('news.urls')),
+    path('statistics/', movieViews.statistics_view, name='statistics'),
+    path('signup/', movieViews.signup, name='signup')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
